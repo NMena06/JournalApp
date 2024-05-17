d389f220edb772f4f55b6571d29c0cb04cc1ccdd
Nico <nico6mena@gmail.com>
2024-05-17 15:19:58 -0300

    NICO - Actualizo pantalla de inico

--- src/journal/views/NothingSelectedView.jsx
+++ src/journal/views/NothingSelectedView.jsx
@@ -1,24 +1,136 @@
-import { Grid, Typography } from '@mui/material';
-import { StarOutline } from '@mui/icons-material';
-
+import { useSelector } from 'react-redux';
+import { Grid, Typography, TextField, Button, IconButton } from '@mui/material';
+import { UploadOutlined, SaveOutlined, DeleteOutline } from '@mui/icons-material';
+import { format } from 'date-fns';
+import { useDispatch } from 'react-redux';
+import { setActiveNote, startDeletingNote, startSaveNote, startUploadingFiles } from '../../store/journal';
+import { ImageGallery } from '../components';
 
 export const NothingSelectedView = () => {
+  const dispatch = useDispatch();
+  const { notes } = useSelector(state => state.journal);
+
+  // Ordenar las notas por fecha en orden descendente
+  const sortedNotes = [...notes].sort((a, b) => new Date(b.date) - new Date(a.date));
+
   return (
     <Grid
       className='animate__animated animate__fadeIn animate__faster'
       container
-      spacing={ 0 }
+      spacing={2}
       direction="column"
       alignItems="center"
-      justifyContent="center"
-      sx={{ minHeight: 'calc(100vh - 110px)', backgroundColor: 'primary.main', borderRadius: 3 }}
+      justifyContent="flex-start"
+      sx={{ minHeight: 'calc(100vh - 110px)', backgroundColor: 'primary.main', borderRadius: 3, padding: 2 }}
     >
-        <Grid item xs={ 12 }>
+      {sortedNotes.length > 0 ? (
+        sortedNotes.map(note => {
+          const { id, title, body, date, imageUrls } = note;
+          const dateString = format(new Date(date), "dd/MM/yyyy HH:mm:ss");
+
+          const handleSaveNote = () => {
+            dispatch(setActiveNote(note));
+            dispatch(startSaveNote());
+          };
+
+          const handleFileInputChange = ({ target }) => {
+            if (target.files.length === 0) return;
+            dispatch(setActiveNote(note));
+            dispatch(startUploadingFiles(target.files));
+          };
+
+          const handleDelete = () => {
+            dispatch(setActiveNote(note));
+            dispatch(startDeletingNote());
+          };
+
+          return (
+            <Grid
+              container
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+              sx={{ mb: 1, backgroundColor: 'white', borderRadius: 2, padding: 2, width: '100%' }}
+              key={id}
+            >
+              <Grid item>
+                <Typography fontSize={39} fontWeight="light">
+                  {dateString}
+                </Typography>
+                {/* <IconButton
+                  color="primary"
+                  onClick={() => document.getElementById(`fileInput-${id}`).click()}
+                >
+                  <UploadOutlined />
+                </IconButton>
+
+                <Button
+                  onClick={handleSaveNote}
+                  color="primary"
+                  sx={{ padding: 2 }}
+                >
+                  <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
+                  Guardar
+                </Button> */}
+              </Grid>
+
+              <Grid container>
+                <TextField
+                  type="text"
+                  variant="filled"
+                  fullWidth
+                  placeholder="Ingrese un título"
+                  label="Título"
+                  sx={{ border: "none", mb: 1 }}
+                  name="title"
+                  value={title}
+                  disabled
+                />
+              </Grid>
+              <ImageGallery images={imageUrls} />
+              <Grid item>
+                <input
+                  type="file"
+                  multiple
+                  id={`fileInput-${id}`}
+                  onChange={handleFileInputChange}
+                  style={{ display: "none" }}
+                />
+              </Grid>
+
+              <Grid container>
+                <TextField
+                  type="text"
+                  variant="filled"
+                  fullWidth
+                  multiline
+                  placeholder="¿Qué sucedió en el día de hoy?"
+                  minRows={5}
+                  name="body"
+                  value={body}
+                  disabled
+                />
+              </Grid>
+
+              <Grid container justifyContent="end">
+                {/* <Button onClick={handleDelete} sx={{ mt: 2 }} color="error">
+                  <DeleteOutline />
+                  Borrar
+                </Button> */}
+              </Grid>
+            </Grid>
+          );
+        })
+      ) : (
+        <>
+          <Grid item xs={12}>
             <StarOutline sx={{ fontSize: 100, color: 'white' }} />
-        </Grid>
-        <Grid item xs={ 12 }>
+          </Grid>
+          <Grid item xs={12}>
             <Typography color="white" variant='h5'>Selecciona o crea una entrada</Typography>
-        </Grid>
+          </Grid>
+        </>
+      )}
     </Grid>
-  )
-}
+  );
+};
